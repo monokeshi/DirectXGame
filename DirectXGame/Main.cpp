bacda0c2c209ad653d8f7c381683379866f1db3f
gitgit__ <kindorudesu21@gmail.com>
2021-07-09 10:00:54 +0900

    DEBUGモード時のみコンソール表示を行うよう変更

--- DirectXGame/Main.cpp
+++ DirectXGame/Main.cpp
@@ -4,7 +4,11 @@
 
 #define DIRECTINPUT_VERSION 0x0800 // DirectInputのバージョン指定
 
+#ifdef _DEBUG
+int main()
+#else
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+#endif
 {
     auto &app = Application::GetInstance();
     if ( !app.Initialize() )
